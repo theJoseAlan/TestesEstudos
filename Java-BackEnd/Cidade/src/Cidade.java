269d9db667737f9b4c48fe72aee41c0983dcbd4e
theJoseAlan <salesalan854@gmail.com>
2022-11-28 19:28:34 -0300

    Cidade

---
+++ Java-BackEnd/Cidade/src/Cidade.java
@@ -0,0 +1,76 @@
+public class Cidade {
+    private int ddd;
+    private String nome;
+
+    private int nro_habitantes;
+    private double renda_per_capita;
+    private boolean capital;
+    private String estado;
+    private String nome_prefeito;
+
+    public Cidade(int ddd, String nome, int nro_habitantes, double renda_per_capita, boolean capital, String estado, String nome_prefeito) {
+        this.ddd = ddd;
+        this.nome = nome;
+        this.nro_habitantes = nro_habitantes;
+        this.renda_per_capita = renda_per_capita;
+        this.capital = capital;
+        this.estado = estado;
+        this.nome_prefeito = nome_prefeito;
+    }
+
+    public int getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(int ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNro_habitantes() {
+        return nro_habitantes;
+    }
+
+    public void setNro_habitantes(int nro_habitantes) {
+        this.nro_habitantes = nro_habitantes;
+    }
+
+    public double getRenda_per_capita() {
+        return renda_per_capita;
+    }
+
+    public void setRenda_per_capita(double renda_per_capita) {
+        this.renda_per_capita = renda_per_capita;
+    }
+
+    public boolean isCapital() {
+        return capital;
+    }
+
+    public void setCapital(boolean capital) {
+        this.capital = capital;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNome_prefeito() {
+        return nome_prefeito;
+    }
+
+    public void setNome_prefeito(String nome_prefeito) {
+        this.nome_prefeito = nome_prefeito;
+    }
+}
---
+++ Java-BackEnd/Cidade/src/CidadeDAO.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CidadeDAO {
+    private Connection conn;
+
+    public CidadeDAO(){
+        this.conn = ConnectionFactory.getConnection();
+    }
+
+    public boolean insereCidade(Cidade cidade){
+        String sql = "INSERT INTO Cidade.cidade(ddd, nome, nro_habitantes, renda_per_capita, capital, estado, nome_prefeito)"+
+                "VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement stmt;
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, cidade.getDdd());
+            stmt.setString(2, cidade.getNome());
+            stmt.setInt(3, cidade.getNro_habitantes());
+            stmt.setDouble(4, cidade.getRenda_per_capita());
+            stmt.setBoolean(5, cidade.isCapital());
+            stmt.setString(6, cidade.getEstado());
+            stmt.setString(7, cidade.getNome_prefeito());
+            stmt.execute();
+            stmt.close();
+            System.out.println("Dado enviado!");
+            return true;
+        }catch (SQLException e){
+            System.err.println("Erro ao enviar dados ao DB");
+            System.err.println(e.getMessage());
+            return false;
+        }
+    }
+
+}
