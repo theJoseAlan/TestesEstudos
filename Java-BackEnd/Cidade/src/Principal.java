cd6c62e16379d6422c8c0f7914195ebf4feea78e
theJoseAlan <salesalan854@gmail.com>
2022-11-29 10:47:38 -0300

    Organização da main

--- Java-BackEnd/Cidade/src/Principal.java
+++ Java-BackEnd/Cidade/src/Principal.java
@@ -1,40 +1,59 @@
 import java.util.Scanner;
 public class Principal {
+    //Metodo para remoção de cidade
+    private static void remocao(Scanner input, CidadeDAO cidadeDao) {
+        System.out.print("Insira o DDD da cidade para removê-la: ");
+        int dddr = input.nextInt();
+        cidadeDao.removeCidade(dddr);
+    }
+
+    //Metodo para adição de cidade
+    private static void insercao(Scanner input, CidadeDAO cidadeDao) {
+        System.out.println("=== INSERÇÃO DE CIDADE ===");
+        System.out.print("DDD: ");
+        int ddd = input.nextInt();
+        input.nextLine();
+        System.out.print("NOME: ");
+        String nome = input.nextLine();
+        System.out.print("NÚMERO DE HABITANTES: ");
+        int nro_habitantes = input.nextInt();
+        System.out.print("RENDA PER CAPITA: ");
+        double renda_per_capita = input.nextDouble();
+        System.out.print("CAPITAL [TRUE/FALSE]? ");
+        boolean capital = input.nextBoolean();
+        input.nextLine();
+        System.out.print("ESTADO: ");
+        String estado = input.nextLine();
+        System.out.print("PREFEITO: ");
+        String prefeito = input.nextLine();
+
+        CidadePOJO cidadePOJOAserInserida = new CidadePOJO(ddd,nome,nro_habitantes,renda_per_capita,capital,estado,prefeito);
+        cidadeDao.insereCidade(cidadePOJOAserInserida);
+    }
+
+
+    //Programa Principal
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         CidadeDAO cidadeDao = new CidadeDAO();
-        System.out.print("1 - ADD cidade\n2 - Remover cidade\nR: ");
-        int op = input.nextInt();
 
-        if(op==1){
+        int op=0;
 
-            System.out.println("=== INSERÇÃO DE CIDADE ===");
-            System.out.print("DDD: ");
-            int ddd = input.nextInt();
-            input.nextLine();
-            System.out.print("NOME: ");
-            String nome = input.nextLine();
-            System.out.print("NÚMERO DE HABITANTES: ");
-            int nro_habitantes = input.nextInt();
-            System.out.print("RENDA PER CAPITA: ");
-            double renda_per_capita = input.nextDouble();
-            System.out.print("CAPITAL [TRUE/FALSE]? ");
-            boolean capital = input.nextBoolean();
-            input.nextLine();
-            System.out.print("ESTADO: ");
-            String estado = input.nextLine();
-            System.out.print("PREFEITO: ");
-            String prefeito = input.nextLine();
+        do {
+            System.out.print("1 - ADD cidade\n2 - Remover cidade\n-1 - Sair\nR: ");
+            op = input.nextInt();
 
-            CidadePOJO cidadePOJOAserInserida = new CidadePOJO(ddd,nome,nro_habitantes,renda_per_capita,capital,estado,prefeito);
-            cidadeDao.insereCidade(cidadePOJOAserInserida);
+            if(op==1){
 
-        } else if (op==2) {
-            System.out.print("Insira o DDD da cidade para removê-la: ");
-            int dddr = input.nextInt();
-            cidadeDao.removeCidade(dddr);
-        }
+                insercao(input, cidadeDao);
 
+            } else if (op==2) {
+                remocao(input, cidadeDao);
+            }
+
+        }while (op!=-1);
+
+        System.out.println("Até a próxima");
         input.close();
 
     }
