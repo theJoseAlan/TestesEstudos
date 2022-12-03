bf373d4fd17fe474bcfaa043dc0703834851170d
theJoseAlan <117518719+theJoseAlan@users.noreply.github.com>
2022-12-03 18:32:50 -0300

    completed

--- mildevs-escola/src/main/java/br/com/mildevs/Programa.java
+++ mildevs-escola/src/main/java/br/com/mildevs/Programa.java
@@ -13,25 +13,22 @@ import java.util.List;
 public class Programa {
     public static void main(String[] args) {
 
-
         ProfessorDAO professorDAO = new ProfessorDAO();
         TurmaDAO turmaDAO = new TurmaDAO();
         AlunoDAO alunoDAO = new AlunoDAO();
 
         Professor professor = new Professor();
-        professor.setCodFuncionario(1);
         professor.setDisciplina("Lógica de programação");
         professor.setNivelGraduacao("Doutorado");
         professor.setNome("Alan");
         professor.setSalario(2000);
         professor.setTelefone("1234567890");
-
-        //professorDAO.criaProfessor(professor);
+        professorDAO.criaProfessor(professor);
 
         Professor professorDb = professorDAO.concultaProfessor(1);
         System.out.println("Professor encontrado -> ");
-        List<Professor> professoresNoDb = professorDAO.listaProfessores();
 
+        List<Professor> professoresNoDb = professorDAO.listaProfessores();
         for(Professor professorEncontradoNaListagem : professoresNoDb){
             System.out.println(professorEncontradoNaListagem);
         }
@@ -42,7 +39,6 @@ public class Programa {
         sala.setLargura(345);
 
         Turma turmaCriada = turmaDAO.criaTurma(sala);
-
         turmaDAO.adcionaProfessor(professorDb,turmaCriada.getCodTurma());
 
     }
