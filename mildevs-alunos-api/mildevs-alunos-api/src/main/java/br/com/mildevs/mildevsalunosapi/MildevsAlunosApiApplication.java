0fa823bc752f4220f8f9e9d1a5d509c0fd43f050
theJoseAlan <salesalan854@gmail.com>
2022-12-19 10:09:25 -0300

    Alunos API Spring

---
+++ mildevs-alunos-api/mildevs-alunos-api/src/main/java/br/com/mildevs/mildevsalunosapi/MildevsAlunosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mildevs.mildevsalunosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MildevsAlunosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MildevsAlunosApiApplication.class, args);
+	}
+
+}
